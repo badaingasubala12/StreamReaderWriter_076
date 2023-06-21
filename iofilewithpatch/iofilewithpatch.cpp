03570ee5aca95f5f57702ac42e9f872b0813810a
badai ngasubala <badaingasubala12@gmail.com>
2023-06-21 11:21:37 +0700

    melakukan perulangan tiap baris, dan menampilkan disini, setelahnya menutup file

--- iofilewithpatch/iofilewithpatch.cpp
+++ iofilewithpatch/iofilewithpatch.cpp
@@ -42,6 +42,7 @@ int main() {
 	// menunjuk ke sebuah file
 	infile.open(Namafile, ios::in);
 
+
 	cout << endl << ">= Membuka dan membaca file " << endl;
 	//jika file ada maka
 	if (infile.is_open())
